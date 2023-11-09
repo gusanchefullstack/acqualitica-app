a8fb5b8e3a0e0821755f8262f22689631ffcd372
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2023-11-09 01:39:27 -0500

    Add error handlers including PrismaClientKnownRequestError

--- backend/v1/middleware/errorHandler.js
+++ backend/v1/middleware/errorHandler.js
@@ -14,14 +14,11 @@ const logError = (error, req, res, next) => {
 const prismaErrors = (error, req, res, next) => {
   if (error instanceof Prisma.PrismaClientKnownRequestError) {
     // The .code property can be accessed in a type-safe manner
-    if (error.code === "P2025") {
-      console.log(`Error code: ${error.code}`);
-      res.json({
-        result: "Failed",
-        error: error.meta.cause,
-        code: error.code,
-      });
-    }
+    res.json({
+      result: "Failed",
+      error: error.meta.cause,
+      code: error.code,
+    });
   } else {
     res.json({
       result: "Failed",
